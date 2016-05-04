60f3d6ee9a7bd530c6468e900cc7bad2c77f1513
Eberhard Wolff <eberhard.wolff@gmail.com>
2016-05-04 12:05:45 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+vagrant up
+cd microservice/microservice-demo
+../../apache-maven-3.3.9/bin/mvn -DskipTests -DdownloadSources=true -DdownloadJavadocs=true -q clean package
+cd ..
+cd ..
+cd user-registration-V2
+cd user-registration-application
+../../apache-maven-3.3.9/bin/mvn -DskipTests -DdownloadSources=true -DdownloadJavadocs=true -q clean package
